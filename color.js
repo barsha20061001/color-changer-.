90c0be352abd52c9dff1d07c76ef6b3223790449
barsha20061001 <barshadgp212@gmail.com>
2025-07-14 20:32:08 +0530

    project

--- color.js
+++ color.js
@@ -6,16 +6,16 @@ buttons.forEach(function (button) {
   button.addEventListener('click', function (e) {
     console.log(e);
     console.log(e.target);
-    if (e.target.id === 'grey') {
+    if (e.target.id === 'palevioletred') {
       body.style.backgroundColor = e.target.id;
     }
     if (e.target.id === 'white') {
       body.style.backgroundColor = e.target.id;
     }
-    if (e.target.id === 'blue') {
+    if (e.target.id === 'blueviolet') {
       body.style.backgroundColor = e.target.id;
     }
-    if (e.target.id === 'yellow') {
+    if (e.target.id === 'olivedrab') {
       body.style.backgroundColor = e.target.id;
     }
     
